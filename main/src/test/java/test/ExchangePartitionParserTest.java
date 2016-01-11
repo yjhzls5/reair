fb6fee23c2c780753e9feec710fc5e667483a820
Paul Yang <paul.yang@airbnb.com>
2016-01-10 22:20:43 -0800

    Add unit test for parsing exchange partition commands

--- main/src/test/java/test/ExchangePartitionParserTest.java
+++ main/src/test/java/test/ExchangePartitionParserTest.java
@@ -1,26 +1,47 @@
 package test;
 
+import com.airbnb.di.hive.common.HiveObjectSpec;
 import com.airbnb.di.hive.replication.ExchangePartitionParser;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ExchangePartitionParserTest {
     private static final Log LOG = LogFactory.getLog(
             ExchangePartitionParserTest.class);
 
     @Test
-    public void testParse() {
-        String query = "ALTER TABLE test_db.test_table_exchange_to EXCHANGE PARTITION(ds='1', hr='2') WITH TABLE test_db.test_table_exchange_from";
-        String query2 = "alter table xp_flog.checkout_flow_event exchange " +
-                "partition (d='2015-09-07') " +
-                "with table flog.checkout_flow_event";
-        String query3 = "ALTER TABLE tmp_brain.extrema_listing_decline_dates_unavailable_20120814 EXCHANGE PARTITION(ds='2012-08-14') WITH TABLE extrema.listing_decline_dates_unavailable";
+    public void testParse1() {
+        String query = "ALTER TABLE test_db.test_table_exchange_to EXCHANGE " +
+                "PARTITION(ds='1', hr='2') " +
+                "WITH TABLE test_db.test_table_exchange_from";
+
         ExchangePartitionParser parser = new ExchangePartitionParser();
         boolean parsed = parser.parse(query);
-        LOG.debug("parsed=" + parsed);
-        LOG.debug("exchangeFromSpec=" + parser.getExchangeFromSpec());
-        LOG.debug("exchangeToSpec=" + parser.getExchangeToSpec());
-        LOG.debug("partitionName=" + parser.getPartitionName());
+        assertTrue(parsed);
+        assertEquals(new HiveObjectSpec("test_db", "test_table_exchange_from"),
+                parser.getExchangeFromSpec());
+        assertEquals(new HiveObjectSpec("test_db", "test_table_exchange_to"),
+                parser.getExchangeToSpec());
+        assertEquals("ds=1/hr=2", parser.getPartitionName());
+    }
+
+    @Test
+    public void testParse2() {
+        String query = "ALTER TABLE test_table_exchange_to EXCHANGE " +
+                "PARTITION(ds='1', hr='2') " +
+                "WITH TABLE test_table_exchange_from";
+
+        ExchangePartitionParser parser = new ExchangePartitionParser();
+        boolean parsed = parser.parse(query);
+        assertTrue(parsed);
+        assertEquals(new HiveObjectSpec("default", "test_table_exchange_from"),
+                parser.getExchangeFromSpec());
+        assertEquals(new HiveObjectSpec("default", "test_table_exchange_to"),
+                parser.getExchangeToSpec());
+        assertEquals("ds=1/hr=2", parser.getPartitionName());
     }
 }
