416daa64e8e9ebf2817353faed8e53a4162c921b
ddavydov <ddavydov@twitter.com>
2016-01-23 14:22:24 -0800

    More test fixes

--- main/src/main/java/com/airbnb/di/hive/batchreplication/hivecopy/MetastoreScanInputFormat.java
+++ main/src/main/java/com/airbnb/di/hive/batchreplication/hivecopy/MetastoreScanInputFormat.java
@@ -5,7 +5,9 @@ import com.google.common.collect.Lists;
 
 import com.airbnb.di.hive.common.HiveMetastoreClient;
 import com.airbnb.di.hive.common.HiveMetastoreException;
-import com.airbnb.di.hive.common.ThriftHiveMetastoreClient;
+import com.airbnb.di.hive.replication.configuration.Cluster;
+import com.airbnb.di.hive.replication.configuration.ClusterFactory;
+import com.airbnb.di.hive.replication.configuration.ConfigurationException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.io.Text;
@@ -37,13 +39,11 @@ public class MetastoreScanInputFormat extends FileInputFormat<Text, Text> {
   private static final int NUMBER_OF_THREADS = 16;
   private static final int NUMBER_OF_MAPPERS = 100;
 
-  public static final String SRC_METASTORE_HOST_CONF = "replication.metastore.src.host";
-  public static final String DST_METASTORE_HOST_CONF = "replication.metastore.dst.host";
-
   @Override
   public RecordReader<Text, Text> createRecordReader(
       InputSplit inputSplit,
-      TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException {
+      TaskAttemptContext taskAttemptContext)
+  throws IOException, InterruptedException {
     return new TableRecordReader();
   }
 
@@ -51,24 +51,21 @@ public class MetastoreScanInputFormat extends FileInputFormat<Text, Text> {
   public List<InputSplit> getSplits(JobContext context) throws IOException {
     // split into pieces, fetching the splits in parallel
     ExecutorService executor = Executors.newCachedThreadPool();
-    List<InputSplit> splits = new ArrayList<>();
-    HiveMetastoreClient client = null;
+    Cluster srcCluster = null;
+    HiveMetastoreClient srcClient = null;
     List<String> allTables = new ArrayList<>();
 
-    String metastoreHost = context.getConfiguration().get(SRC_METASTORE_HOST_CONF);
-    if (metastoreHost == null) {
-      throw new IOException("Invalid metastore host name.");
-    }
-
-    String[] parts = metastoreHost.split(":");
     try {
-      client = new ThriftHiveMetastoreClient(parts[0], Integer.valueOf(parts[1]));
-    } catch (HiveMetastoreException e) {
-      throw new IOException(e);
+      ClusterFactory clusterFactory =
+          MetastoreReplUtils.createClusterFactory(context.getConfiguration());
+      srcCluster = clusterFactory.getSrcCluster();
+      srcClient = srcCluster.getMetastoreClient();
+    } catch (ConfigurationException | HiveMetastoreException e) {
+      throw new IOException("Invalid metastore host name.", e);
     }
 
     try {
-      List<String> databases = client.getAllDatabases();
+      List<String> databases = srcClient.getAllDatabases();
       LOG.info("Total dbs: " + databases.size());
 
       List<Future<List<String>>> splitfutures = new ArrayList<>();
@@ -77,8 +74,8 @@ public class MetastoreScanInputFormat extends FileInputFormat<Text, Text> {
 
       for (List<String> range : Lists.partition(databases, dbPerThread)) {
         // for each range, pick a live owner and ask it to compute bite-sized splits
-        splitfutures
-            .add(executor.submit(new SplitCallable(range, parts[0], Integer.valueOf(parts[1]))));
+        splitfutures.add(executor.submit(
+              new SplitCallable(range, srcClient)));
       }
 
       // wait until we have all the results back
@@ -97,7 +94,7 @@ public class MetastoreScanInputFormat extends FileInputFormat<Text, Text> {
       throw new IOException(e);
     } finally {
       executor.shutdownNow();
-      client.close();
+      srcClient.close();
     }
 
     assert allTables.size() > 0;
@@ -105,7 +102,8 @@ public class MetastoreScanInputFormat extends FileInputFormat<Text, Text> {
 
     final int tablesPerSplit = Math.max(allTables.size() / NUMBER_OF_MAPPERS, 1);
 
-    return Lists.transform(Lists.partition(allTables, tablesPerSplit),
+    return Lists.transform(
+        Lists.partition(allTables, tablesPerSplit),
         new Function<List<String>, InputSplit>() {
           @Override
           public InputSplit apply(@Nullable List<String> tables) {
@@ -118,21 +116,18 @@ public class MetastoreScanInputFormat extends FileInputFormat<Text, Text> {
    * Get list of folders. Find next level of folders and return.
    */
   class SplitCallable implements Callable<List<String>> {
-    private final String host;
-    private final int port;
+    private final HiveMetastoreClient client;
     private final List<String> candidates;
 
 
-    public SplitCallable(List<String> candidates, String host, int port) {
+    public SplitCallable(List<String> candidates, HiveMetastoreClient client) {
       this.candidates = candidates;
-      this.host = host;
-      this.port = port;
+      this.client = client;
     }
 
     public List<String> call() throws Exception {
       ArrayList<String> tables = new ArrayList<>();
-      HiveMetastoreClient client = new ThriftHiveMetastoreClient(host, port);
-      for (final String db : candidates) {
+      for (final String db: candidates) {
         tables.addAll(Lists.transform(client.getAllTables(db), new Function<String, String>() {
           @Override
           public String apply(String str) {
@@ -142,8 +137,8 @@ public class MetastoreScanInputFormat extends FileInputFormat<Text, Text> {
       }
       client.close();
 
-      LOG.info("Thread " + Thread.currentThread().getId() + ":processed " + candidates.size()
-          + " dbs. Produced " + tables.size() + " tables.");
+      LOG.info("Thread " + Thread.currentThread().getId() + ":processed "
+          + candidates.size() + " dbs. Produced " + tables.size() + " tables.");
 
       return tables;
     }
