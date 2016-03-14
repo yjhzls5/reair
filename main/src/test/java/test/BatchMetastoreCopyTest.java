575aa546e3a412ee9e9d7680dcb042d1c8f76afd
Jingwei Lu <jingwei_lu@jingwei-lu.local>
2016-03-14 13:43:30 -0700

    fix a bad rebase

--- main/src/test/java/test/BatchMetastoreCopyTest.java
+++ main/src/test/java/test/BatchMetastoreCopyTest.java
@@ -26,7 +26,6 @@ import java.sql.SQLException;
  */
 public class BatchMetastoreCopyTest extends MockClusterTest {
 
-<<<<<<< 81c6c39d7c2aa095f52600d10a5d373a4b4d83e8
   /**
    * TODO.
    *
@@ -118,133 +117,50 @@ public class BatchMetastoreCopyTest extends MockClusterTest {
 
     assertTrue(!ReplicationUtils.exists(destMetastore, partitionSpec2));
   }
-=======
-    @BeforeClass
-    public static void setupClass() throws IOException, SQLException {
-        MockClusterTest.setupClass();
-   }
 
-    @Test
-    public void testCopyNewTables() throws Exception {
-        // Create an unpartitioned table in the source
-        HiveObjectSpec spec = new HiveObjectSpec("test", "test_table");
-        Table srcTable = ReplicationTestUtils.createUnpartitionedTable(conf,
-                srcMetastore,
-                spec,
-                TableType.MANAGED_TABLE,
-                srcWarehouseRoot);
 
-        // Create a partitioned table in the source
-        HiveObjectSpec tableSpec = new HiveObjectSpec("test", "partitioned_table");
-        Table srcTable2 = ReplicationTestUtils.createPartitionedTable(conf,
-                srcMetastore,
-                tableSpec,
-                TableType.MANAGED_TABLE,
-                srcWarehouseRoot);
+  @Test
+  public void testHdfsCopy() throws Exception {
+    // Create an unpartitioned table in the source
+    HiveObjectSpec spec = new HiveObjectSpec("test", "test_table");
+    Table srcTable = ReplicationTestUtils.createUnpartitionedTable(conf,
+        srcMetastore,
+        spec,
+        TableType.MANAGED_TABLE,
+        srcWarehouseRoot);
 
-        // Create several partitions in the source table
-        HiveObjectSpec partitionSpec1 = new HiveObjectSpec("test",
-                "partitioned_table", "ds=1/hr=1");
-        HiveObjectSpec partitionSpec2 = new HiveObjectSpec("test",
-                "partitioned_table", "ds=1/hr=2");
-        HiveObjectSpec partitionSpec3 = new HiveObjectSpec("test",
-                "partitioned_table", "ds=1/hr=3");
+    // Create a partitioned table in the source
+    HiveObjectSpec tableSpec = new HiveObjectSpec("test", "partitioned_table");
+    Table srcTable2 = ReplicationTestUtils.createPartitionedTable(conf,
+        srcMetastore,
+        tableSpec,
+        TableType.MANAGED_TABLE,
+        srcWarehouseRoot);
 
-        Partition srcPartition1 = ReplicationTestUtils.createPartition(conf,
-                srcMetastore, partitionSpec1);
-        Partition srcPartition2 = ReplicationTestUtils.createPartition(conf,
-                srcMetastore, partitionSpec2);
-        Partition srcPartition3 = ReplicationTestUtils.createPartition(conf,
-                srcMetastore, partitionSpec3);
+    // Create several partitions in the source table
+    HiveObjectSpec partitionSpec1 = new HiveObjectSpec("test",
+        "partitioned_table", "ds=1/hr=1");
+    HiveObjectSpec partitionSpec2 = new HiveObjectSpec("test",
+        "partitioned_table", "ds=1/hr=2");
+    HiveObjectSpec partitionSpec3 = new HiveObjectSpec("test",
+        "partitioned_table", "ds=1/hr=3");
 
-        JobConf jobConf = new JobConf(conf);
+    Partition srcPartition1 = ReplicationTestUtils.createPartition(conf,
+        srcMetastore, partitionSpec1);
+    Partition srcPartition2 = ReplicationTestUtils.createPartition(conf,
+        srcMetastore, partitionSpec2);
+    Partition srcPartition3 = ReplicationTestUtils.createPartition(conf,
+        srcMetastore, partitionSpec3);
 
-        String[] args = {};
-        jobConf.set(DeployConfigurationKeys.BATCH_JOB_OUTPUT_DIR,
-                new Path(destCluster.getFsRoot(), "test_output").toString());
-        jobConf.set(DeployConfigurationKeys.BATCH_JOB_CLUSTER_FACTORY_CLASS, MockClusterFactory.class.getName());
+    JobConf jobConf = new JobConf(conf);
 
-        ToolRunner.run(jobConf, new MetastoreReplicationJob(), args);
+    String[] args = {"--source", srcWarehouseRoot.toString(),
+        "--destination", destWarehouseRoot.toString(),
+        "--output-path", new Path(destCluster.getFsRoot(), "test_output").toString(),
+        "--operation", "a,d,u"};
 
-        assertTrue(ReplicationUtils.exists(destMetastore, spec));
+    ToolRunner.run(jobConf, new ReplicationJob(), args);
 
-        Table dstTable = destMetastore.getTable(spec.getDbName(), spec.getTableName());
-        assertTrue(directoryCopier.equalDirs(new Path(srcTable.getSd().getLocation()),
-                new Path(dstTable.getSd().getLocation())));
-
-        assertTrue(ReplicationUtils.exists(destMetastore, partitionSpec1));
-        assertTrue(ReplicationUtils.exists(destMetastore, partitionSpec2));
-        assertTrue(ReplicationUtils.exists(destMetastore, partitionSpec3));
-
-        Partition dstPartition1 = destMetastore.getPartition(partitionSpec1.getDbName(),
-                partitionSpec1.getTableName(),
-                partitionSpec1.getPartitionName());
-        assertTrue(directoryCopier.equalDirs(new Path(srcPartition1.getSd().getLocation()),
-                new Path(dstPartition1.getSd().getLocation())));
-
-        Partition dstPartition2 = destMetastore.getPartition(partitionSpec2.getDbName(),
-                partitionSpec2.getTableName(),
-                partitionSpec2.getPartitionName());
-        assertTrue(directoryCopier.equalDirs(new Path(srcPartition2.getSd().getLocation()),
-                new Path(dstPartition2.getSd().getLocation())));
-
-        Partition dstPartition3 = destMetastore.getPartition(partitionSpec3.getDbName(),
-                partitionSpec3.getTableName(),
-                partitionSpec3.getPartitionName());
-        assertTrue(directoryCopier.equalDirs(new Path(srcPartition3.getSd().getLocation()),
-                new Path(dstPartition3.getSd().getLocation())));
-
-        ReplicationTestUtils.dropTable(srcMetastore, spec);
-        ReplicationTestUtils.dropPartition(srcMetastore, partitionSpec2);
-
-        ToolRunner.run(jobConf, new MetastoreReplicationJob(), args);
-
-        assertTrue(!ReplicationUtils.exists(destMetastore, partitionSpec2));
-    }
-
-    @Test
-    public void testHdfsCopy() throws Exception {
-        // Create an unpartitioned table in the source
-        HiveObjectSpec spec = new HiveObjectSpec("test", "test_table");
-        Table srcTable = ReplicationTestUtils.createUnpartitionedTable(conf,
-                srcMetastore,
-                spec,
-                TableType.MANAGED_TABLE,
-                srcWarehouseRoot);
-
-        // Create a partitioned table in the source
-        HiveObjectSpec tableSpec = new HiveObjectSpec("test", "partitioned_table");
-        Table srcTable2 = ReplicationTestUtils.createPartitionedTable(conf,
-                srcMetastore,
-                tableSpec,
-                TableType.MANAGED_TABLE,
-                srcWarehouseRoot);
-
-        // Create several partitions in the source table
-        HiveObjectSpec partitionSpec1 = new HiveObjectSpec("test",
-                "partitioned_table", "ds=1/hr=1");
-        HiveObjectSpec partitionSpec2 = new HiveObjectSpec("test",
-                "partitioned_table", "ds=1/hr=2");
-        HiveObjectSpec partitionSpec3 = new HiveObjectSpec("test",
-                "partitioned_table", "ds=1/hr=3");
-
-        Partition srcPartition1 = ReplicationTestUtils.createPartition(conf,
-                srcMetastore, partitionSpec1);
-        Partition srcPartition2 = ReplicationTestUtils.createPartition(conf,
-                srcMetastore, partitionSpec2);
-        Partition srcPartition3 = ReplicationTestUtils.createPartition(conf,
-                srcMetastore, partitionSpec3);
-
-        JobConf jobConf = new JobConf(conf);
-
-        String[] args = {"--source", srcWarehouseRoot.toString(),
-                "--destination", destWarehouseRoot.toString(),
-                "--output-path", new Path(destCluster.getFsRoot(), "test_output").toString(),
-                "--operation", "a,d,u"};
-
-        ToolRunner.run(jobConf, new ReplicationJob(), args);
-
-        assertTrue(directoryCopier.equalDirs(srcWarehouseRoot,destWarehouseRoot));
-    }
->>>>>>> improve hdfscopy according to comments
+    assertTrue(directoryCopier.equalDirs(srcWarehouseRoot, destWarehouseRoot));
+  }
 }
